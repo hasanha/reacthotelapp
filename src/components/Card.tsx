532607a22f6b5e2db28161c54f1829786ac74cfd
hasanha <hasan.hammadeh7@gmail.com>
2023-03-26 13:01:05 -0400

    xx

---
+++ src/components/Card.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='card'>
+      <h2 className='title'>All Hotels</h2>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/lib/firebase.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAU5l6gtbljL691Q6QSc944JTHfclqfnMI",
+  authDomain: "hotel-dashboard-475b8.firebaseapp.com",
+  projectId: "hotel-dashboard-475b8",
+  storageBucket: "hotel-dashboard-475b8.appspot.com",
+  messagingSenderId: "52850099547",
+  appId: "1:52850099547:web:c0020ef2db33959f57c67e",
+  measurementId: "G-5XR5LLY6Z8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
